b41a2c5a4f31ccda7909176f9ecaec36aad75dff
Gianmarco Brunialti <gianmarco.brunialti@liferay.com>
2024-10-02 09:46:08 +0200

    LPD-32224 LPD-32233 Commerce Order and Order items views and handlers

--- modules/apps/commerce/commerce-order-content-web/src/main/java/com/liferay/commerce/order/content/web/internal/fragment/renderer/OrdersDataSetFragmentRenderer.java
+++ modules/apps/commerce/commerce-order-content-web/src/main/java/com/liferay/commerce/order/content/web/internal/fragment/renderer/OrdersDataSetFragmentRenderer.java
@@ -226,11 +226,11 @@ public class OrdersDataSetFragmentRenderer implements FragmentRenderer {
 				_jsonFactory.createJSONObject();
 
 			commerceOrderTypeJSONObject.put(
-				"name_i18n", commerceOrderType.getName(
-					_portal.getLocale(httpServletRequest))
-				).put(
-					"orderTypeId", commerceOrderType.getCommerceOrderTypeId()
-				);
+				"name_i18n",
+				commerceOrderType.getName(_portal.getLocale(httpServletRequest))
+			).put(
+				"orderTypeId", commerceOrderType.getCommerceOrderTypeId()
+			);
 
 			commerceOrderTypesJSONArray.put(commerceOrderTypeJSONObject);
 		}
@@ -308,7 +308,8 @@ public class OrdersDataSetFragmentRenderer implements FragmentRenderer {
 
 		if (fdsName.equals(CommerceOrderFragmentFDSNames.PENDING_ORDERS)) {
 			return HashMapBuilder.<String, Object>put(
-				"accountId", () -> {
+				"accountId",
+				() -> {
 					long groupId = _portal.getScopeGroupId(httpServletRequest);
 
 					AccountEntry accountEntry =
---
+++ modules/apps/commerce/commerce-order-content-web/src/main/resources/META-INF/resources/js/order_data_set/data_renderers/ProductURLDataRenderer.js
@@ -0,0 +1,36 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import React from 'react';
+
+import {MiniCartUtils} from 'commerce-frontend-js';
+import ClayLink from "@clayui/link";
+
+const ProductURLDataRenderer = ({
+	additionalProps: {
+		siteDefaultURL,
+		productURLSeparator,
+	},
+	itemData: {
+		productURLs,
+	},
+	value,
+}) => {
+	const productURL = MiniCartUtils.generateProductPageURL(
+		siteDefaultURL, productURLs, productURLSeparator);
+
+	return (
+		<div className="table-list-title">
+			<ClayLink
+				data-senna-off
+				href={productURL}
+			>
+				{value}
+			</ClayLink>
+		</div>
+	);
+};
+
+export default ProductURLDataRenderer;
