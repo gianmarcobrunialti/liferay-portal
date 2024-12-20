a529676dd1d183f46a77a01eed65fae22ea5ee2c
Gianmarco Brunialti <gianmarco.brunialti@liferay.com>
2024-12-20 12:56:25 +0100

    trigger site initializers

--- workspaces/liferay-commerceminium4-workspace/client-extensions/liferay-commerceminium4-site-initializer/site-initializer/fragments/group/commerce/fragments/personal-bar/index.js
+++ workspaces/liferay-commerceminium4-workspace/client-extensions/liferay-commerceminium4-site-initializer/site-initializer/fragments/group/commerce/fragments/personal-bar/index.js
@@ -3,12 +3,26 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
-const signInButton = fragmentElement.querySelector('button.sign-in');
+const setGuestOrderEnabled = ({guestOrderEnabled}) => {
+	const signInButton = document.querySelector('button.sign-in');
 
-if (signInButton) {
-	window.Liferay.on('perform-sign-in', () => {
-		if (signInButton) {
-			signInButton.click();
-		}
-	});
-}
+	if (signInButton && guestOrderEnabled) {
+		const clonedSignInButton = signInButton.cloneNode(true);
+
+		signInButton.parentElement.replaceChild(clonedSignInButton, signInButton);
+		const performGuestSignIn = (event) => {
+			event.preventDefault();
+
+			window.Liferay.fire('perform-sign-in');
+
+			return false;
+		};
+
+		clonedSignInButton.addEventListener('click', performGuestSignIn);
+	}
+
+	window.Liferay.detach('guest-order-enabled', setGuestOrderEnabled);
+};
+
+window.Liferay.on('guest-order-enabled', setGuestOrderEnabled);
+
