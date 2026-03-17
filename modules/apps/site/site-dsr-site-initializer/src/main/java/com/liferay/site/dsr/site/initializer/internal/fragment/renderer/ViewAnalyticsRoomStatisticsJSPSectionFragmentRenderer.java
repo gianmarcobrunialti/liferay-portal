c8df14b8e66fef148916b580ca5b754f71d7d144
Gianmarco Brunialti <gianmarco.brunialti@liferay.com>
2026-03-17 11:47:26 +0100

    leftovers

---
+++ modules/apps/site/site-dsr-site-initializer/src/main/java/com/liferay/site/dsr/site/initializer/internal/fragment/renderer/ViewAnalyticsRoomStatisticsJSPSectionFragmentRenderer.java
@@ -0,0 +1,60 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.site.dsr.site.initializer.internal.fragment.renderer;
+
+import com.liferay.fragment.renderer.FragmentRenderer;
+import com.liferay.fragment.renderer.FragmentRendererContext;
+import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.object.service.ObjectEntryService;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import com.liferay.site.dsr.site.initializer.internal.display.context.ViewAnalyticsRoomStatisticsSectionDisplayContext;
+import jakarta.servlet.http.HttpServletRequest;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Gianmarco Brunialti Masera
+ */
+@Component(service = FragmentRenderer.class)
+public class ViewAnalyticsRoomStatisticsJSPSectionFragmentRenderer
+	extends BaseJSPSectionFragmentRenderer<ViewAnalyticsRoomStatisticsSectionDisplayContext> {
+
+	@Override
+	public String getLabelKey() {
+		return "room-statistics";
+	}
+
+	@Override
+	protected ViewAnalyticsRoomStatisticsSectionDisplayContext getDisplayContext(
+		FragmentRendererContext fragmentRendererContext, HttpServletRequest httpServletRequest) {
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		return new ViewAnalyticsRoomStatisticsSectionDisplayContext(
+			httpServletRequest,
+			_objectDefinitionLocalService.
+				fetchObjectDefinitionByExternalReferenceCode(
+					"L_DSR_ROOM", themeDisplay.getCompanyId()),
+			_objectEntryService);
+	}
+
+	@Override
+	protected String getJSPPath() {
+		return "/view_analytics_room_statistics.jsp";
+	}
+
+
+	@Reference
+	private ObjectDefinitionLocalService _objectDefinitionLocalService;
+
+	@Reference
+	private ObjectEntryService _objectEntryService;
+
+}
